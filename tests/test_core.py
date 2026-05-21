7c574aac514b0c39d417b00158d9cde0de83c961
Kooroush Masoumi <kooroushmasoumi@gmail.com>
2026-05-21 21:21:11 +0330

    write readme quickstart

--- tests/test_core.py
+++ tests/test_core.py
@@ -155,3 +155,5 @@ def test_time_budget_exceeded():
 
 
 def test_token_bucket_basic_acquire():
+    bucket = TokenBucketRateLimiter(capacity=3, refill_per_second=1.0)
+    assert bucket.try_acquire() is True
